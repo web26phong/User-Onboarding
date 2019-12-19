939c9642ac9d913456d15b844c13c2bb20cb5256
Phong Yang <phongyang88@hotmail.com>
2019-12-19 16:10:05 -0600

    added form validation and error messages

--- user-onboarding/src/Styles.js
+++ user-onboarding/src/Styles.js
@@ -13,4 +13,17 @@ export const StyledLabel = styled.label`
 
 export const FieldSection = styled.div`
     display: flex;
+    color: black;
+`;
+
+export const FormSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 1%;
+    text-align: left;
+    color: red;
+`;
+
+export const FormContainer = styled.div`
+    display: flex;
 `;
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,50 +1,71 @@
-import React from "react";
-import {FormStyle, FieldSection, StyledLabel} from "../Styles";
-import {withFormik, Form, Field} from "formik";
+import React, {useState, useEffect} from "react";
+import {FormStyle, FieldSection, StyledLabel, FormSection, FormContainer} from "../Styles";
+import {withFormik, Form, Field, yupToFormErrors} from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-const UserForm = ({values}) => {
+const UserForm = ({values, errors, touched, status}) => {
     return (
         <div>
             <Form>
-                <FormStyle>
+                <FormContainer>
+                    <FormSection>
+                        <FormStyle>
+                            <FieldSection>
+                                <StyledLabel htmlFor="name">Name: </StyledLabel>
+                                <Field id="name" type="text" name="name" placeholder="enter name here"/>
+                            </FieldSection>
 
-                    <FieldSection>
-                        <StyledLabel htmlFor="name">Name: </StyledLabel>
-                        <Field id="name" type="text" name="name" placeholder="enter name here"/>
-                    </FieldSection>
+                            <FieldSection>
+                                <StyledLabel htmlFor="email">Email: </StyledLabel>
+                                <Field id="email" type="email" name="email" placeholder="enter email here"/>
+                            </FieldSection>
 
-                    <FieldSection>
-                        <StyledLabel htmlFor="email">Email: </StyledLabel>
-                        <Field id="email" type="email" name="email" placeholder="enter email here"/>
-                    </FieldSection>
+                            <FieldSection>
+                                <StyledLabel htmlFor="password">Password: </StyledLabel>
+                                <Field id="password" type="password" name="password" placeholder="enter password here"/>
+                            </FieldSection>
 
-                    <FieldSection>
-                        <StyledLabel htmlFor="password">Password: </StyledLabel>
-                        <Field id="password" type="password" name="password" placeholder="enter password here"/>
-                    </FieldSection>
+                            <FieldSection>
+                                <label htmlFor="ToS">I understand and accept the Terms of Service: </label>
+                                <Field id="ToS" type="checkbox" name="ToS" check={values.ToS}/>
+                            </FieldSection>
 
-                    <FieldSection>
-                       <label htmlFor="ToS">I understand and accept the Terms of Service: </label>
-                        <Field id="ToS" type="checkbox" name="ToS" check={values.ToS}/>
-                    </FieldSection>
+                            <FieldSection>
+                                <button type="submit">Submit</button> 
+                            </FieldSection>
+                        </FormStyle>
+                </FormSection>
 
-                    <FieldSection>
-                    <button>Submit</button> 
-                    </FieldSection>
-
-                </FormStyle>
+                <FormSection>
+                        {touched.name && errors.name && (<span>{errors.name}</span>)}
+                        {touched.email && errors.email && (<span>{errors.email}</span>)}
+                        {touched.password && errors.password && (<span>{errors.password}</span>)}
+                        {touched.ToS && errors.ToS && (<span>{errors.ToS}</span>)}
+                </FormSection>
+                </FormContainer>
+                
             </Form>
         </div>
     );
 }
 
 const FormikUserForm = withFormik({
-    mapPropsToValues({}){
+    mapPropsToValues({name, email, password, ToS}){
         return {
-
-        }
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            ToS: ToS || false
+        };
     },
 
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("A name is required."),
+        email: Yup.string().required("A valid email address is required."),
+        password: Yup.string().required("A password is required."),
+        ToS: Yup.boolean().oneOf([true], "You must accept the Terms of Service to continue.")
+    })
 })(UserForm);
 
 export default FormikUserForm;
